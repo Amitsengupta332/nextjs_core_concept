d7f9df3b31f4649428d9e7f545363560466823af
amitsengupta <amitsengupta332@gmail.com>
2025-09-19 00:14:27 +0600

    image optimization v-1

---
+++ src/app/gallery/page.js
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+const GalleryPage = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-center"> Image Optimzation</h1>
+
+      <h1 className="text-3xl text-center">Regular Image tag</h1>
+      <img
+        src="https://i.ibb.co.com/fG2jjsbN/0221d67b-88f5-4109-bf43-8e1f82b5f7ca.jpg"
+        alt="Image Optimzation"
+         width={500}
+        height={500}
+        className="mx-auto"
+      />
+
+      <h1 className="text-3xl text-center py-1 my-1">Next Image tag</h1>
+      <Image
+        src="https://i.ibb.co.com/fG2jjsbN/0221d67b-88f5-4109-bf43-8e1f82b5f7ca.jpg"
+        alt="Image Optimzation"
+        width={500}
+        height={500}
+        className="mx-auto"
+      />
+    </div>
+  );
+};
+
+export default GalleryPage;
